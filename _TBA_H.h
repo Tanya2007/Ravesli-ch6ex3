e3df1cd37ea84fbff2f929cc25717d11d3b5ffdd
Tanya2007 <122219831+Tanya2007@users.noreply.github.com>
2023-08-09 10:37:33 +0300

    Update _TBA_H.h

--- _TBA_H.h
+++ _TBA_H.h
@@ -1,13 +1,14 @@
 #ifndef _TBA_H
 #define _TBA_H
 
+#include <string>
+
 #define UTF8_encoded_file	1						//Indicates that the source file is encoded in utf8 
 													//(1 if yes, and 0 if no (i.e. if the file encoding is in ANSI)).
 
 #if defined (_WIN32) && UTF8_encoded_file
 #include <windows.h>
 #include <vector>
-#include <string>
 #define _TBA UTF8_to_CP1251
 
 std::string UTF8_to_CP1251(const std::string&);
